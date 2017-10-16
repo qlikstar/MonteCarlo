096bf0159ae3275f2ede213426108540409e4f12
qlikstar <sanket.mishra@citrix.com>
2017-10-15 20:54:27 -0700

    Adding UnitTests

---
+++ src/test/java/com/decipherx/fintech/montecarlo/controller/MainRestControllerTest.java
@@ -0,0 +1,19 @@
+package com.decipherx.fintech.montecarlo.controller;
+
+import com.decipherx.fintech.montecarlo.service.Simulator;
+import com.decipherx.fintech.montecarlo.util.Utility;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class MainRestControllerTest {
+
+    @Test
+    public void getPortfolioDetails(){
+
+        return;
+    }
+
+}
